a02e333f979fdeb22e4b89b21ace5f5ea4fe15ac
Pedro Pessoa <pedropessoaramos@gmail.com>
2022-03-31 00:31:19 -0300

    root import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {useColorScheme} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
-import Themes from './theme';
-import Home from './pages/Home';
+import Themes from '~/theme';
+import Home from '~/pages/Home';
 
 const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
