424947f10fa10c34e9d78f08dd9079a82e21875c
Pedro Pessoa <pedropessoaramos@gmail.com>
2022-04-03 17:08:42 -0300

    Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {useColorScheme} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import Themes from '~/theme';
-import Home from '~/pages/Home';
+import Routes from './routes';
 
 const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <ThemeProvider theme={isDarkMode ? Themes.dark : Themes.light}>
-      <Home />
+      <Routes />
     </ThemeProvider>
   );
 };
