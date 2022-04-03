8c3aa5a6f3d8c4285d1b8436c85d2ac1320d1022
Pedro Pessoa <pedropessoaramos@gmail.com>
2022-04-03 17:41:25 -0300

    ajuste no tema

--- src/theme.ts
+++ src/theme.ts
@@ -2,11 +2,23 @@ type ITheme = {
   variant: string;
   colors: {
     background: string;
+    backgroundSecondary: string;
     primary: string;
     secondary: string;
     text: string;
   };
   spacing: number[];
+  texto: {
+    tamanhos: {
+      h1: number;
+      h2: number;
+      h3: number;
+      h4: number;
+      h5: number;
+      p: number;
+      small: number;
+    };
+  };
 };
 type IThemes = {
   dark: {
@@ -21,22 +33,46 @@ const themes: IThemes = {
   dark: {
     variant: 'dark',
     colors: {
-      background: '#0ff',
-      primary: '#00beee',
-      secondary: '#8e8e8e',
+      background: '#F4F7F8',
+      backgroundSecondary: '#FFFFFF',
+      primary: '#4B9093',
+      secondary: '#F59A73',
       text: '#000',
     },
     spacing: [8, 16, 24, 32, 40, 48, 56, 64],
+    texto: {
+      tamanhos: {
+        h1: 24,
+        h2: 22,
+        h3: 20,
+        h4: 18,
+        h5: 16,
+        p: 14,
+        small: 12,
+      },
+    },
   },
   light: {
     variant: 'light',
     colors: {
-      background: '#ff0',
-      primary: '#00beee',
-      secondary: '#8e8e8e',
+      background: '#F4F7F8',
+      backgroundSecondary: '#FFFFFF',
+      primary: '#4B9093',
+      secondary: '#F59A73',
       text: '#000',
     },
     spacing: [8, 16, 24, 32, 40, 48, 56, 64],
+    texto: {
+      tamanhos: {
+        h1: 24,
+        h2: 22,
+        h3: 20,
+        h4: 18,
+        h5: 16,
+        p: 14,
+        small: 12,
+      },
+    },
   },
 };
 
