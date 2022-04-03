6cb1094d10fdc3b4130d7f2c930bcf0e670bdf14
Pedro Pessoa <pedropessoaramos@gmail.com>
2022-04-03 18:27:10 -0300

    lib de icones

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import * as S from './styles';
 
 const Home: React.FC = () => {
-  return <S.Container />;
+  return (
+    <S.Container>
+      <Icon name="rocket" size={30} color="#900" />
+    </S.Container>
+  );
 };
 
 export default Home;
