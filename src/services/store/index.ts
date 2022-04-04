e230a2aad8e100a09f1cfbc6e93ebae8fc1b5c25
Pedro Pessoa <pedropessoaramos@gmail.com>
2022-04-03 21:07:24 -0300

    ajuste no redux

--- src/services/store/index.ts
+++ src/services/store/index.ts
@@ -2,7 +2,13 @@ import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit';
 import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import rootReducer from './reducers';
 
-const store = configureStore({reducer: rootReducer});
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
