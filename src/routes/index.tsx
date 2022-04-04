f9ffc836ea2dc7a88ae9c124c45464451e7d7973
Pedro Pessoa <pedropessoaramos@gmail.com>
2022-04-03 21:07:38 -0300

    style do header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from '~/pages/Home';
 import Form from '~/pages/Form';
+import {useTheme} from 'styled-components';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -13,9 +14,18 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Routes: React.FC = () => {
+  const theme = useTheme();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {backgroundColor: theme.colors.primary},
+          headerTintColor: theme.colors.backgroundSecondary,
+          headerBackTitleVisible: false,
+          headerTitleAlign: 'center',
+          title: 'Usuários',
+        }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Form" component={Form} />
       </Stack.Navigator>
